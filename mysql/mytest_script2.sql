b00170a933b9c30b63bbc0c7335ffad1db838f29
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-07 20:05:49 +0530

    Update mytest_script2.sql

--- mysql/mytest_script2.sql
+++ mysql/mytest_script2.sql
@@ -5,7 +5,7 @@ CREATE TABLE customer2 (
   custname VARCHAR(100)
 );
 
-INSERT INTO customer (id, custname) VALUES
+INSERT INTO customer2 (id, custname) VALUES
 (1, 'ABC Co'),
 (2, 'XYZ Corp'),
 (3, 'BN Plumbing');
