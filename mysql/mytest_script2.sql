977d02ef298fd30eb39196c0b406e9320a128ed8
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-07 19:59:08 +0530

    Create mytest_script2.sql

---
+++ mysql/mytest_script2.sql
@@ -0,0 +1,11 @@
+USE sql12659958;
+
+CREATE TABLE customer2 (
+  id INT,
+  custname VARCHAR(100)
+);
+
+INSERT INTO customer (id, custname) VALUES
+(1, 'ABC Co'),
+(2, 'XYZ Corp'),
+(3, 'BN Plumbing');
