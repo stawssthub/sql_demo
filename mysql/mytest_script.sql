280891ed53438ce0dc23e8dcd8e769b798f39cbe
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-07 20:23:29 +0530

    Update mytest_script.sql

--- mysql/mytest_script.sql
+++ mysql/mytest_script.sql
@@ -2,16 +2,6 @@
 
 USE sql12659958;
 
-CREATE TABLE customer (
-  id INT,
-  custname VARCHAR(100)
-);
-
-INSERT INTO customer (id, custname) VALUES
-(4, 'ABC Co'),
-(2, 'XYZ Corp'),
-(3, 'BN Plumbing');
-
 CREATE TABLE customer3 (
   emp_id INT,
   custnames VARCHAR(100)
