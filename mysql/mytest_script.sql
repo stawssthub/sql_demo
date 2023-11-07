d40cb5b632b5e0bc987fcdf558b888b86fb45064
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-07 20:13:59 +0530

    Update mytest_script.sql

--- mysql/mytest_script.sql
+++ mysql/mytest_script.sql
@@ -11,3 +11,13 @@ INSERT INTO customer (id, custname) VALUES
 (4, 'ABC Co'),
 (2, 'XYZ Corp'),
 (3, 'BN Plumbing');
+
+CREATE TABLE customer3 (
+  emp_id INT,
+  custnames VARCHAR(100)
+);
+
+INSERT INTO customer3 (id, custname) VALUES
+(1, 'ABC Co'),
+(2, 'XYZ Corp'),
+(3, 'BN Plumbing');
