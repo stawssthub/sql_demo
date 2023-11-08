e7a79e14c87a81dae1fce1cba9fdc5de83ad7706
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-08 11:50:40 +0530

    Update test_script.sql

--- mysql/test_script.sql
+++ mysql/test_script.sql
@@ -1,11 +1,11 @@
 USE sql12659958;
 
-CREATE TABLE customer (
+CREATE TABLE customer4 (
   id INT,
   custname VARCHAR(100)
 );
 
-INSERT INTO customer (id, custname) VALUES
-(4, 'ABC Co'),
+INSERT INTO customer4 (id, custname) VALUES
+(1, 'ABC Co'),
 (2, 'XYZ Corp'),
 (3, 'BN Plumbing');
