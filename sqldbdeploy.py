3add3e8bb4940fcd0d2dece02b2cbbe3b3ceafbf
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-08 13:56:10 +0530

    Update sqldbdeploy.py

--- sqldbdeploy.py
+++ sqldbdeploy.py
@@ -29,15 +29,15 @@ for x in cursor:
 #cursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
 #cursor.execute("CREATE TABLE customers2 (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
 
-directory_path = "./mysql/"
+directory_path = "mysql/"
 
 # read the  .sql file
 for filename in os.listdir(directory_path):
     if filename.endswith(".sql"):
-        script_path = os.path.join(directory_path, filename)
+        script_paths = os.path.join(directory_path, filename)
         
 # Read the SQL script from the file
-with open('script_path', 'r') as sql_file:
+with open(script_paths, 'r') as sql_file:
     result_iterator = cursor.execute(sql_file.read(), multi=True)
     for res in result_iterator:
         print("Running query: ", res)  # Will print out a short representation of the query
