ab112e8595c0bb54dbffe787f9ad682acd150d19
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-08 15:52:14 +0530

    Update sqldbdeploy.py

--- sqldbdeploy.py
+++ sqldbdeploy.py
@@ -38,18 +38,14 @@ for filename in os.listdir(directory_path):
         print(f"Processing: {script_paths}")
 # Read the SQL script from the file
 with open(script_paths, 'r') as sql_file:
-    sql_script = sql_file.read()
-    
-    cursor.execute(sql_script)
-    
-    #result_iterator = cursor.execute(sql_file.read())
-    #print(result_iterator)
-    #for res in result_iterator:
-        #print("Running query: ", res)  # Will print out a short representation of the query
-        #print(f"Affected {res.rowcount} rows" )
+    result_iterator = cursor.execute(sql_file.read())
+    print(result_iterator)
+    for res in result_iterator:
+        print("Running query: ", res)  # Will print out a short representation of the query
+        print(f"Affected {res.rowcount} rows" )
  
 # commit the changes to the database 
-#connection.commit() 
+connection.commit() 
  
 # close the cursor and connection 
 cursor.close() 
