2a0d85bcf85bf2fb23039cb4c223a2266177ef06
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-08 13:12:28 +0530

    Create sqldbdeploy.py

---
+++ sqldbdeploy.py
@@ -0,0 +1,44 @@
+import os
+import mysql.connector
+import glob
+
+# Database connection parameters
+db_params = {
+    "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT"),
+    "database": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+}
+
+# Establish a database connection
+connection = mysql.connector.connect(**db_params)
+cursor = connection.cursor()
+
+cursor.execute("SHOW DATABASES")
+
+for D in cursor:
+  print(D)
+    
+cursor.execute("SHOW TABLES")
+
+for x in cursor:
+  print(x)
+    
+#cursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
+#cursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
+#cursor.execute("CREATE TABLE customers2 (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
+
+# read the contents of the .sql file
+with open('mysql/test_script.sql', 'r') as sql_file:
+    result_iterator = cursor.execute(sql_file.read(), multi=True)
+    for res in result_iterator:
+        print("Running query: ", res)  # Will print out a short representation of the query
+        print(f"Affected {res.rowcount} rows" )
+ 
+# commit the changes to the database 
+connection.commit() 
+ 
+# close the cursor and connection 
+cursor.close() 
+connection.close() 
