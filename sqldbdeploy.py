b3619e2e78fb0cc5e273518797fce402ac31bc4b
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-08 16:04:29 +0530

    Update sqldbdeploy.py

--- sqldbdeploy.py
+++ sqldbdeploy.py
@@ -30,23 +30,28 @@ for x in cursor:
 #cursor.execute("CREATE TABLE customers2 (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
 
 directory_path = "mysql/"
-
+try:
+    cursor.execute("START TRANSACTION")
 # read the  .sql file
-for filename in os.listdir(directory_path):
-    if filename.endswith(".sql"):
-        script_paths = os.path.join(directory_path, filename)
-        print(f"Processing: {script_paths}")
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".sql"):
+            script_paths = os.path.join(directory_path, filename)
+            print(f"Processing: {script_paths}")
 # Read the SQL script from the file
-with open(script_paths, 'r') as sql_file:
-    result_iterator = cursor.execute(sql_file.read(), multi=True)
-    print(result_iterator)
-    for res in result_iterator:
-        print("Running query: ", res)  # Will print out a short representation of the query
-        print(f"Affected {res.rowcount} rows" )
+            with open(script_paths, 'r') as sql_file:
+            result_iterator = cursor.execute(sql_file.read(), multi=True)
+            print(result_iterator)
+            for res in result_iterator:
+            print("Running query: ", res)  # Will print out a short representation of the query
+            print(f"Affected {res.rowcount} rows" )
  
 # commit the changes to the database 
-connection.commit() 
- 
+    connection.commit() 
+except Exception as e:
+    # Handle exceptions, roll back the transaction, and log the error
+    connection.rollback()
+    print(f"Error: {e}")
+finally:
 # close the cursor and connection 
-cursor.close() 
-connection.close() 
+    cursor.close() 
+    connection.close() 
