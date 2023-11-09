404eaf7f3fc44ae258c74c66cdcb1091778c346b
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-09 17:31:11 +0530

    Update sqldbdeploy.py

--- sqldbdeploy.py
+++ sqldbdeploy.py
@@ -53,9 +53,9 @@ try:
         #sql_statements = sql_file.read().split(';')  # Split SQL statements by semicolon
            result_iterator = cursor.execute(sql_file.read(), multi=True)
            print(result_iterator)
-         for res in result_iterator:
-            print("Running query: ", res)  # Will print out a short representation of the query
-            print(f"Affected {res.rowcount} rows" )
+           for res in result_iterator:
+                print("Running query: ", res)  # Will print out a short representation of the query
+                print(f"Affected {res.rowcount} rows" )
             
 # commit the changes to the database 
     connection.commit()
