b878cc936f29440bfd9b0d1bb75253415856ff15
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-09 13:42:33 +0530

    Update test2_script.sql
    test

--- mysql/test2_script.sql
+++ mysql/test2_script.sql
@@ -1,12 +1,11 @@
 USE sql12659958;
 
-#CREATE TABLE test_table2(
-        #3id INT PRIMARY KEY AUTO_INCREMENT,
-        #3test_field VARCHAR(250) NOT NULL DEFAULT ''
-#);
+CREATE TABLE test_table2(
+        3id INT PRIMARY KEY AUTO_INCREMENT,
+        3test_field VARCHAR(250) NOT NULL DEFAULT ''
+);
 
 
-#INSERT INTO test_table2(test_field) VALUES('test1');
-#INSERT INTO test_table2(test_field) VALUES('test2');
-#INSERT INTO test_table2(test_field) VALUES('test3');
-INSERT INTO test_table2(test_field) VALUES('test535');
+INSERT INTO test_table2(test_field) VALUES('test1');
+INSERT INTO test_table2(test_field) VALUES('test2');
+INSERT INTO test_table2(test_field) VALUES('test3');
