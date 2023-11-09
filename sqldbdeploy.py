cddc93b3220da445485232cdf3721f45ba174723
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-09 13:23:22 +0530

    Update sqldbdeploy.py

--- sqldbdeploy.py
+++ sqldbdeploy.py
@@ -1,8 +1,8 @@
+
 import os
 import subprocess
 import mysql.connector
 import glob
-import git
 
 # Database connection parameters
 db_params = {
@@ -13,6 +13,13 @@ db_params = {
     "password": os.getenv("DB_PASSWORD"),
 }
 
+#last_commit_sha = subprocess.check_output("git rev-parse HEAD", shell=True).decode("utf-8").strip()
+#changed_files = glob.glob('mysql/*.sql')
+
+# Iterate through the list of changed SQL files and print them
+#for file in changed_files:
+    #print(file)
+    
 # Establish a database connection
 connection = mysql.connector.connect(**db_params)
 cursor = connection.cursor()
@@ -26,14 +33,35 @@ cursor.execute("SHOW TABLES")
 
 for x in cursor:
   print(x)
+    
 
-changed_files = subprocess.check_output(['git', 'diff', '--name-only', 'HEAD^', 'HEAD'], universal_newlines=True)
-changed_files = [file.strip() for file in changed_files.split('\n') if file.endswith(".sql")]
-for file_path in changed_files:
-    deploy_sql_changes(cursor, file_path)
+#directory_path = "mysql/"
+#try:
+    #cursor.execute("START TRANSACTION")
+# Use Git to get the list of changed SQL files
+#git_command = git diff --name-only HEAD~1 HEAD -- '*.sql'
 
-    # Commit changes
-    connection.commit()
+last_commit_sha = subprocess.check_output("git rev-parse HEAD", shell=True).decode("utf-8").strip()
 
-cursor.close()
-connection.close()
+print(f"last_commit: {last_commit_sha}")
+
+git_command = f"git diff --name-only HEAD~1 {last_commit_sha} -- '*.sql'"
+
+print(f"Executing command: {git_command}")
+
+changed_files = subprocess.check_output(git_command, shell=True).decode("utf-8").strip().split("\n")
+
+for file in changed_files:
+    with open(file, "r") as sql_file:
+        sql_statements = sql_file.read().split(';')  # Split SQL statements by semicolon
+        for sql_statement in sql_statements:
+        # Execute the SQL statement against the database
+            cursor.execute(sql_statement)
+            
+# commit the changes to the database 
+    connection.commit() 
+    
+
+# close the cursor and connection 
+    cursor.close() 
+    connection.close() 
