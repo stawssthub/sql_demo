e50aeb240aafb0db9fe4f41df45303aa690ae08e
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-09 15:05:19 +0530

    Update sqldbdeploy.py

--- sqldbdeploy.py
+++ sqldbdeploy.py
@@ -52,11 +52,11 @@ print(f"Executing command: {git_command}")
 changed_files = subprocess.check_output(git_command, shell=True).decode("utf-8").strip().split("\n")
 
 
- with open(file, "r") as sql_file:
+    with open(file, "r") as sql_file:
         #sql_statements = sql_file.read().split(';')  # Split SQL statements by semicolon
         result_iterator = cursor.execute(sql_file.read(), multi=True)
         print(result_iterator)
- for res in result_iterator:
+    for res in result_iterator:
         print("Running query: ", res)  # Will print out a short representation of the query
         print(f"Affected {res.rowcount} rows" )
             
