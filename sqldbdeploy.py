b2b53484b9db7b0697b7fcbcc371462508b80646
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-09 16:41:05 +0530

    Update sqldbdeploy.py

--- sqldbdeploy.py
+++ sqldbdeploy.py
@@ -54,8 +54,8 @@ changed_files = subprocess.check_output(git_command, shell=True).decode("utf-8")
 
 
 for file in changed_files:
-try:
-    cursor.execute("START TRANSACTION")
+    try:
+        cursor.execute("START TRANSACTION")
     with open(file, "r") as sql_file:
         #sql_statements = sql_file.read().split(';')  # Split SQL statements by semicolon
         result_iterator = cursor.execute(sql_file.read(), multi=True)
