a4e1c7a826447f8c0a2de9e02f13bef9a8f647cb
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-09 17:01:32 +0530

    Update sqldbdeploy.py

--- sqldbdeploy.py
+++ sqldbdeploy.py
@@ -31,14 +31,12 @@ for x in cursor:
 
 #directory_path = "mysql/"
 
-try:
-    cursor.execute("START TRANSACTION")
 
 # Use Git to get the list of changed SQL files
 #git_command = git diff --name-only HEAD~1 HEAD -- '*.sql'
 
 last_commit_sha = subprocess.check_output("git rev-parse HEAD", shell=True).decode("utf-8").strip()
-except Exception as p:
+
 print(f"last_commit: {last_commit_sha}")
 
 git_command = f"git diff --name-only HEAD~1 {last_commit_sha} -- '*.sql'"
@@ -61,12 +59,8 @@ for file in changed_files:
             
 # commit the changes to the database 
     connection.commit() 
-except Exception as e:
-    # Handle exceptions, roll back the transaction, and log the error
-    connection.rollback()
-    print(f"Error: {e}")  
+ 
 
-finally:
 
 # close the cursor and connection 
     cursor.close() 
