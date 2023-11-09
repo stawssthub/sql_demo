0a49d14de139b0d33cabea16effbb2743f4aa784
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-09 13:11:51 +0530

    Update sqldbdeploy.py

--- sqldbdeploy.py
+++ sqldbdeploy.py
@@ -29,7 +29,6 @@ for x in cursor:
 
 changed_files = subprocess.check_output(['git', 'diff', '--name-only', 'HEAD^', 'HEAD'], universal_newlines=True)
 changed_files = [file.strip() for file in changed_files.split('\n') if file.endswith(".sql")]
-
     for file_path in changed_files:
         deploy_sql_changes(cursor, file_path)
 
