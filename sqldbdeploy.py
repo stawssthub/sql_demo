2f76a4825c69db0effa249418f96ff51159867d2
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-09 17:30:02 +0530

    Update sqldbdeploy.py

--- sqldbdeploy.py
+++ sqldbdeploy.py
@@ -53,7 +53,7 @@ try:
         #sql_statements = sql_file.read().split(';')  # Split SQL statements by semicolon
            result_iterator = cursor.execute(sql_file.read(), multi=True)
            print(result_iterator)
-        for res in result_iterator:
+         for res in result_iterator:
             print("Running query: ", res)  # Will print out a short representation of the query
             print(f"Affected {res.rowcount} rows" )
             
