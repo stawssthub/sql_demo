899377388eb9528e0d2062ef1678a0395ff7f743
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-09 17:28:35 +0530

    Update sqldbdeploy.py

--- sqldbdeploy.py
+++ sqldbdeploy.py
@@ -43,25 +43,26 @@ git_command = f"git diff --name-only HEAD~1 {last_commit_sha} -- '*.sql'"
 
 print(f"Executing command: {git_command}")
 
+try:
+    cursor.execute("START TRANSACTION")
+    changed_files = subprocess.check_output(git_command, shell=True).decode("utf-8").strip().split("\n")
 
-changed_files = subprocess.check_output(git_command, shell=True).decode("utf-8").strip().split("\n")
 
-
-for file in changed_files:
-   
-    with open(file, "r") as sql_file:
+    for file in changed_files:
+       with open(file, "r") as sql_file:
         #sql_statements = sql_file.read().split(';')  # Split SQL statements by semicolon
-        result_iterator = cursor.execute(sql_file.read(), multi=True)
-        print(result_iterator)
-    for res in result_iterator:
-        print("Running query: ", res)  # Will print out a short representation of the query
-        print(f"Affected {res.rowcount} rows" )
+           result_iterator = cursor.execute(sql_file.read(), multi=True)
+           print(result_iterator)
+        for res in result_iterator:
+            print("Running query: ", res)  # Will print out a short representation of the query
+            print(f"Affected {res.rowcount} rows" )
             
 # commit the changes to the database 
-    connection.commit() 
- 
-
-
+    connection.commit()
+except Exception as e:
+    connection.rollback()
+    print(f"Error: {e}")
+finally:
 # close the cursor and connection 
     cursor.close() 
     connection.close() 
