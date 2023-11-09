488ed5aea35c41d7e3a80a7b0fd7983f4b2190da
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-09 17:50:49 +0530

    Update test3_script.sql

--- mysql/test3_script.sql
+++ mysql/test3_script.sql
@@ -9,4 +9,3 @@ USE sql12659958;
 #INSERT INTO test_table3(test_field) VALUES('test1');
 #INSERT INTO test_table3(test_field) VALUES('test2');
 #INSERT INTO test_table3(test_field) VALUES('test3');
-INSERT INTO test_table3(test_field) VALUES('test6');
