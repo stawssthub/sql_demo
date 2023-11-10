f62a04334dc1d82bcb97387032b17fccb2015f51
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-10 11:28:33 +0530

    Update test2_script.sql

--- mysql/test2_script.sql
+++ mysql/test2_script.sql
@@ -1,9 +1,9 @@
 USE sql12659958;
 
-/*CREATE TABLE test_table2(
+CREATE TABLE IF NOT EXISTS Stest_table2(
         id INT PRIMARY KEY AUTO_INCREMENT,
         test_field VARCHAR(250) NOT NULL DEFAULT ''
-); */
+);
 
 
 INSERT INTO test_table2(test_field) VALUES('test1');
