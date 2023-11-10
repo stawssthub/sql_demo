08f85920405134452165300a1ad9d5febf13eff0
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-10 11:14:06 +0530

    Update test2_script.sql

--- mysql/test2_script.sql
+++ mysql/test2_script.sql
@@ -3,12 +3,12 @@ USE sql12659958;
 /*CREATE TABLE test_table2(
         id INT PRIMARY KEY AUTO_INCREMENT,
         test_field VARCHAR(250) NOT NULL DEFAULT ''
-); 
+); */
 
 
 INSERT INTO test_table2(test_field) VALUES('test1');
 INSERT INTO test_table2(test_field) VALUES('test2');
 INSERT INTO test_table2(test_field) VALUES('test3');
-INSERT INTO test_table2(test_field) VALUES('test4');*/
 INSERT INTO test_table2(test_field) VALUES('test4');
+INSERT INTO test_table2(test_field) VALUES('test5');
 
