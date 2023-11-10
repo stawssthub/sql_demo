87b7785d9bf05c465b94d7ecad8507e41d152921
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-10 13:51:24 +0530

    Update sqldbdeploy.py

--- sqldbdeploy.py
+++ sqldbdeploy.py
@@ -22,9 +22,9 @@ db_params = [{
 
     
 # Establish a database connection
-for databases in db_params:
-    connection = mysql.connector.connect(**databases)
-    cursor = connection.cursor()
+#for databases in db_params:
+    #connection = mysql.connector.connect(**databases)
+    #cursor = connection.cursor()
 
     # To get the list of databases
     cursor.execute("SHOW DATABASES")
@@ -39,36 +39,39 @@ for databases in db_params:
 
 # Use Git to get the list of changed SQL files
 #git_command = git diff --name-only HEAD~1 HEAD -- '*.sql'
-try:
-    cursor.execute("START TRANSACTION")
-    last_commit_sha = subprocess.check_output("git rev-parse HEAD", shell=True).decode("utf-8").strip()
+for config in db_params:
+    try:
+        connection = mysql.connector.connect(**config)
+        cursor = connection.cursor()
+        cursor.execute("START TRANSACTION")
+        last_commit_sha = subprocess.check_output("git rev-parse HEAD", shell=True).decode("utf-8").strip()
 
-    print(f"last_commit: {last_commit_sha}")
+        print(f"last_commit: {last_commit_sha}")
 
-    git_command = f"git diff --name-only HEAD~1 {last_commit_sha} -- '*.sql'"
+        git_command = f"git diff --name-only HEAD~1 {last_commit_sha} -- '*.sql'"
 
-    print(f"Executing command: {git_command}")
+        print(f"Executing command: {git_command}")
 
 #try:
     #cursor.execute("START TRANSACTION")
-    changed_files = subprocess.check_output(git_command, shell=True).decode("utf-8").strip().split("\n")
+        changed_files = subprocess.check_output(git_command, shell=True).decode("utf-8").strip().split("\n")
 
 
-    for file in changed_files:
-       with open(file, "r") as sql_file:
+        for file in changed_files:
+           with open(file, "r") as sql_file:
         #sql_statements = sql_file.read().split(';')  # Split SQL statements by semicolon
-           result_iterator = cursor.execute(sql_file.read(), multi=True)
-           print(result_iterator)
-           for res in result_iterator:
-                print("Running query: ", res)  # Will print out a short representation of the query
-                print(f"Affected {res.rowcount} rows" )
+               result_iterator = cursor.execute(sql_file.read(), multi=True)
+               print(result_iterator)
+               for res in result_iterator:
+                    print("Running query: ", res)  # Will print out a short representation of the query
+                    print(f"Affected {res.rowcount} rows" )
             
 # commit the changes to the database 
-    connection.commit()
-except Exception as e:
-    connection.rollback()
-    print(f"Error: {e}")
-finally:
+        connection.commit()
+    except Exception as e:
+        connection.rollback()
+        print(f"Error: {e}")
+    finally:
 # close the cursor and connection 
-    cursor.close() 
-    connection.close() 
+        cursor.close() 
+        connection.close() 
