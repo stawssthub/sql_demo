d547f8bcbf093a962ac43588cc4750aa18b7c3aa
Sunil Talluri <sunil.talluri@perficient.com>
2023-11-10 13:20:29 +0530

    test sql files

--- mysql/test4_script.sql
+++ mysql/test4_script.sql
@@ -1,12 +1,13 @@
 USE sql12659958;
 
-/*CREATE TABLE test_table4(
+CREATE TABLE IF NOT EXISTS test_table4(
         id INT PRIMARY KEY AUTO_INCREMENT,
         test_field VARCHAR(250) NOT NULL DEFAULT ''
-);
+)
 
 
-INSERT INTO test_table4(test_field) VALUES('test1');
+/*INSERT INTO test_table4(test_field) VALUES('test1');
 INSERT INTO test_table4(test_field) VALUES('test2');
-INSERT INTO test_table4(test_field) VALUES('test4');*/
-INSERT INTO test_table4(test_field) VALUES('test6');
+INSERT INTO test_table4(test_field) VALUES('test4');
+INSERT INTO test_table4(test_field) VALUES('test6');*/
+INSERT INTO test_table4(test_field) VALUES('test7');
--- mysql/test6_script.sql
+++ mysql/test6_script.sql
@@ -1,11 +1,12 @@
 USE sql12660740;
 
-CREATE TABLE test_table6(
+CREATE TABLE IF NOT EXISTS test_table6(
         id INT PRIMARY KEY AUTO_INCREMENT,
         test_field VARCHAR(250) NOT NULL DEFAULT ''
 );
 
 
-INSERT INTO test_table6(test_field) VALUES('test1');
-INSERT INTO test_table6(test_field) VALUES('test2');
-INSERT INTO test_table6(test_field) VALUES('test3');
+#INSERT INTO test_table6(test_field) VALUES('test1');
+#INSERT INTO test_table6(test_field) VALUES('test2');
+#INSERT INTO test_table6(test_field) VALUES('test3');
+INSERT INTO test_table6(test_field) VALUES('test4');
