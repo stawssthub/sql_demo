363c73c4f6fbdcc27fb2342c3e8387bda07f785f
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-10 12:09:47 +0530

    Create test6_script.sql

---
+++ mysql/test6_script.sql
@@ -0,0 +1,11 @@
+USE sql12660740;
+
+CREATE TABLE test_table6(
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        test_field VARCHAR(250) NOT NULL DEFAULT ''
+);
+
+
+INSERT INTO test_table6(test_field) VALUES('test1');
+INSERT INTO test_table6(test_field) VALUES('test2');
+INSERT INTO test_table6(test_field) VALUES('test3');
