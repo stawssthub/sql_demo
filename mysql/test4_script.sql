e446e8e51d1c85f4c98018d5157202b6437ae4c3
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-14 13:47:34 +0530

    Update test4_script.sql

--- mysql/test4_script.sql
+++ mysql/test4_script.sql
@@ -3,7 +3,7 @@ USE sql12659958;
 CREATE TABLE IF NOT EXISTS test_table4(
         id INT PRIMARY KEY AUTO_INCREMENT,
         test_field VARCHAR(250) NOT NULL DEFAULT ''
-)
+);
 
 
 #INSERT INTO test_table4(test_field) VALUES('test1');
