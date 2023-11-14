180400791176a75320012490a2a7b352a7538254
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-14 15:50:29 +0530

    Update test6_script.sql

--- mysql/test6_script.sql
+++ mysql/test6_script.sql
@@ -1,4 +1,4 @@
-USE sql12660740;
+
 
 CREATE TABLE IF NOT EXISTS test_table6(
         id INT PRIMARY KEY AUTO_INCREMENT,
@@ -9,4 +9,4 @@ CREATE TABLE IF NOT EXISTS test_table6(
 #INSERT INTO test_table6(test_field) VALUES('test1');
 #INSERT INTO test_table6(test_field) VALUES('test2');
 #INSERT INTO test_table6(test_field) VALUES('test3');
-INSERT INTO test_table6(test_field) VALUES('test6');
+INSERT INTO test_table6(test_field) VALUES('test17');
