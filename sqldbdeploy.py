e6002a964338de110ee248b37966eedfc953cfa6
stawssthub <139438824+stawssthub@users.noreply.github.com>
2023-11-14 11:34:29 +0530

    Update sqldbdeploy.py

--- sqldbdeploy.py
+++ sqldbdeploy.py
@@ -23,21 +23,20 @@ database_configs = [{
 
     
 # Establish a database connection
-#for databases in db_params:
-    #connection = mysql.connector.connect(**databases)
-    #cursor = connection.cursor()
+for databases in db_params:
+    connection = mysql.connector.connect(**databases)
+    cursor = connection.cursor()
 
     # To get the list of databases
-    #cursor.execute("SHOW DATABASES")
-    #for D in cursor:
-      #print(D)
+    cursor.execute("SHOW DATABASES")
+    for D in cursor:
+      print(D)
 
     # To get the list of Tables in databases
-    #cursor.execute("SHOW TABLES")
-    #for T in cursor:
-      #print(T)
+    cursor.execute("SHOW TABLES")
+    for T in cursor:
+      print(T)
 
-connection_pool = pooling.MySQLConnectionPool(pool_name="pool", pool_size=2, pool_reset_session=True, **database_configs[0])
 last_commit_sha = subprocess.check_output("git rev-parse HEAD", shell=True).decode("utf-8").strip()
 print(f"last_commit: {last_commit_sha}")
 
@@ -46,35 +45,37 @@ print(f"Executing command: {git_command}")
 
 # Use Git to get the list of changed SQL files
 #git_command = git diff --name-only HEAD~1 HEAD -- '*.sql'
-for config in database_configs:
-    try:
-        #connection = mysql.connector.connect(**config)
-        connection = connection_pool.get_connection()
-        cursor = connection.cursor()
 
-        try:
-            cursor.execute("START TRANSACTION")
-            changed_files = subprocess.check_output(git_command, shell=True).decode("utf-8").strip().split("\n")
+try:
+    cursor.execute("START TRANSACTION")
+    last_commit_sha = subprocess.check_output("git rev-parse HEAD", shell=True).decode("utf-8").strip()
+
+    print(f"last_commit: {last_commit_sha}")
+
+    git_command = f"git diff --name-only HEAD~1 {last_commit_sha} -- '*.sql'"
+
+    print(f"Executing command: {git_command}")
+
+#try:
+    #cursor.execute("START TRANSACTION")
+    changed_files = subprocess.check_output(git_command, shell=True).decode("utf-8").strip().split("\n")
 
 
-            for file in changed_files:
-               with open(file, "r") as sql_file:
+    for file in changed_files:
+       with open(file, "r") as sql_file:
         #sql_statements = sql_file.read().split(';')  # Split SQL statements by semicolon
-                   result_iterator = cursor.execute(sql_file.read(), multi=True)
-                   print(result_iterator)
-               for res in result_iterator:
-                    print("Running query: ", res)  # Will print out a short representation of the query
-                    print(f"Affected {res.rowcount} rows" )
+           result_iterator = cursor.execute(sql_file.read(), multi=True)
+           print(result_iterator)
+           for res in result_iterator:
+                print("Running query: ", res)  # Will print out a short representation of the query
+                print(f"Affected {res.rowcount} rows" )
             
-        # commit the changes to the database 
-            connection.commit()
-        except Exception as e:
-            connection.rollback()
-            print(f"Error: {e}")
-            
-    except mysql.connector.Error as e:
-        print(f"Error in database {config['database']}: {e}")
-    finally:
+# commit the changes to the database 
+    connection.commit()
+except Exception as e:
+    connection.rollback()
+    print(f"Error: {e}")
+finally:
 # close the cursor and connection 
-        cursor.close() 
-        connection.close() 
+    cursor.close() 
+    connection.close()
